778bd0534a44eeeefe0fc9e034556b93d1e085e5
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2022-05-04 09:49:08 +0300

    setting CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth import auth, scheme
 from auth.token import get_current_user
@@ -9,6 +10,14 @@ app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(chat_views.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
