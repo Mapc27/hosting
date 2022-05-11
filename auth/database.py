bb10c6be75c34108cc4cec170f476adc3ed17ef1
Mapc <mapctrigotixd@gmail.com>
2022-05-11 17:00:54 +0300

    = optimized imports

--- auth/database.py
+++ auth/database.py
@@ -2,7 +2,6 @@ from typing import Any, Union
 
 from sqlalchemy.orm import Session
 
-from core import models
 from core.models import User
 from auth.hashed import get_password_hash
 from auth.scheme import UserCreate
@@ -10,7 +9,7 @@ from auth.scheme import UserCreate
 
 def create_user(db: Session, user: UserCreate) -> User:
     hashed = get_password_hash(user.password)
-    db_user: User = models.User(
+    db_user: User = User(
         password=hashed,
         email=user.email,
     )
@@ -21,5 +20,5 @@ def create_user(db: Session, user: UserCreate) -> User:
 
 
 def get_user_by_email(db: Session, email: Union[str, None, Any]) -> User:
-    user: User = db.query(models.User).filter(models.User.email == email).first()
+    user: User = db.query(User).filter(User.email == email).first()
     return user
