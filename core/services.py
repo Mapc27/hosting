5bf3318399ce7c2b8934aa32caac9de64b9f9332
Mapc <mapctrigotixd@gmail.com>
2022-05-29 22:47:42 +0300

    + create and delete housing image

--- core/services.py
+++ core/services.py
@@ -1,10 +1,127 @@
+import os
+import uuid
+from typing import Union
+
+from fastapi import UploadFile
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
-from core.models import Chat, User
+from app.settings import MEDIA_URL
+from core.models import Chat, User, Housing, HousingImage
 
 
-def create_chat_(user1: User, user2: User, db: Session) -> int:
+def get_chat_short_(user_id: int, chat_id: int, db: Session) -> dict:
+    sql_statement = f"""
+        select json_build_object(
+        'chat_id', chat.id,
+        'companion', json_build_object('id', "user".id, 'name', "user".name, 'surname', "user".surname, '_phone_number',
+                 "user"._phone_number, 'phone_country_code', "user".phone_country_code, 'email', "user".email, 'image',
+                  "user".image),
+        'last_message', chat_message,
+        'unread_message_count', (select count(id) from chat_message where chat_message.chat_id = chat.id
+         and chat_message.user_id != {user_id} and chat_message.read = false)
+            )
+        from chat left join chat_message on chat.id = chat_message.chat_id, "user"
+        where chat.id = {chat_id} and {user_id} in (chat.user1_id, chat.user2_id) and
+         "user".id in (chat.user1_id, chat.user2_id) and {user_id} != "user".id
+        and (chat_message.created_at is null or chat_message.created_at =
+        (select max (chat_message.created_at) from chat_message where chat_id = chat.id));
+    """
+    result = db.execute(sql_statement)
+    result_dict = {}
+    for i in result:
+        result_dict = i[0]
+        break
+    return result_dict
+
+
+def create_chat_(user1: User, user2: User, db: Session) -> Union[Chat, None]:
+    if (
+        db.query(Chat)
+        .filter(Chat.user1_id == user1.id, Chat.user2_id == user2.id)
+        .first()
+    ):
+        return None
+
     chat: Chat = Chat(user1_id=user1.id, user2_id=user2.id)
     db.add(chat)
     db.commit()
-    return chat.id
+    return chat
+
+
+def get_housing(user: User, housing_id: int, db: Session) -> Union[None, Housing]:
+    if not user:
+        return None
+    housing: Housing = (
+        db.query(Housing)
+        .filter(Housing.user_id == user.id, Housing.id == housing_id)
+        .first()
+    )
+    return housing
+
+
+async def save_image(file_path: str, image: UploadFile) -> None:
+    contents = await image.read()
+    with open(file_path, "wb") as file:
+        file.write(contents)
+
+
+def replace_main_housing_image(
+    housing_image: HousingImage, housing: Housing, db: Session
+) -> None:
+    db.rollback()
+    main_image = (
+        db.query(HousingImage)
+        .filter(HousingImage.is_main == True, HousingImage.housing_id == housing.id)
+        .first()
+    )
+    main_image.is_main = None
+    db.add(housing_image)
+    db.commit()
+
+
+async def create_housing_image_(
+    image: UploadFile, housing: Housing, db: Session, is_main: Union[None, bool]
+) -> HousingImage:
+    file_name = f'{uuid.uuid4()}.{image.filename.split(".")[-1]}'
+    housing_image: HousingImage = HousingImage(
+        housing=housing, is_main=is_main, file_name=file_name
+    )
+    db.add(housing_image)
+
+    try:
+        db.commit()
+    # image with is_main = True already exists, so move is_main parameter ...
+    except IntegrityError:
+        replace_main_housing_image(housing_image, housing, db)
+
+    file_path = f"../{MEDIA_URL}housings/{housing_image.file_name}"
+    await save_image(file_path, image)
+
+    return housing_image
+
+
+def delete_housing_image_(
+    housing_id: int, image_id: int, db: Session
+) -> Union[HousingImage, None]:
+    housing_image: HousingImage = (
+        db.query(HousingImage)
+        .filter(HousingImage.id == image_id, HousingImage.housing_id == housing_id)
+        .first()
+    )
+    if not housing_image:
+        return None
+    file_path = f"../{MEDIA_URL}housings/{housing_image.file_name}"
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        print(FileNotFoundError)
+
+    db.delete(housing_image)
+    db.commit()
+    if housing_image.is_main:
+        db.query(HousingImage).filter(
+            HousingImage.housing_id == housing_image.housing_id
+        ).first().is_main = True
+    db.commit()
+    return housing_image
