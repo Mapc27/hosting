9aa6149fa1ab11df4a8d3ef9a71c6b5eac79a912
Mapc <mapctrigotixd@gmail.com>
2022-05-31 20:06:53 +0300

    for deploy

--- core/views.py
+++ core/views.py
@@ -190,3 +190,8 @@ def create_house(
     create_housing_comfort(comfort, housing, db)
 
     create_housing_pricing(housing_pricing_scheme, housing, db)
+
+
+@router.get("/test")
+def test() -> str:
+    return "123"
